d393b95eda5f960d23ae95e04f380cafc70fcb64
sree <anilchejarla143@gmail.com>
2017-09-16 16:20:01 +0530

    this is jenkins nokia

---
+++ src/main/java/nokia1/nokia.java
@@ -0,0 +1,11 @@
+class mul 
+  {
+    public static void main(String[] sree)
+    {
+     int num,mul;
+     num= 10;
+     System.out.println("the value of num: "+num);
+    mul=num*num;
+    System.out.println("the value of num: "+mul);
+    }
+}
